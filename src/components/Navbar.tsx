5d31b2d7089c8b970696b3b1c6bf9b831717f8a1
3TERRRNITY <qertisww@gmail.com>
2024-06-11 17:51:52 +0300

    add authorization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,14 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import React from 'react'
 import { buttonVariants } from './ui/button'
 import { ArrowRightIcon } from 'lucide-react'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-const Navbar = () => {
-	// Mock
-	const user = undefined
-	const isAdmin = false
+const Navbar = async () => {
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+
+	const isAdmin = user?.email === process.env.ADMIN_EMAIL
+
 	return (
 		<nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
 			<MaxWidthWrapper>
@@ -75,16 +78,18 @@ const Navbar = () => {
 
 								<div className='h-8 w-px bg-zinc-200 hidden sm:block' />
 
-								<Link
-									href='/configure/upload'
-									className={buttonVariants({
-										size: 'sm',
-										className: 'hidden sm:flex items-center gap-1',
-									})}
-								>
-									Create case
-									<ArrowRightIcon className='ml-1.5 h-5 w-5' />
-								</Link>
+								{user && (
+									<Link
+										href='/configure/upload'
+										className={buttonVariants({
+											size: 'sm',
+											className: 'hidden sm:flex items-center gap-1',
+										})}
+									>
+										Create case
+										<ArrowRightIcon className='ml-1.5 h-5 w-5' />
+									</Link>
+								)}
 							</React.Fragment>
 						)}
 					</div>
